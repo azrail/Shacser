527ace6ccd94f14461c01c7dd804264f810586bd
Sascha Hanke <lethargicprince@googlemail.com>
2011-03-26 22:26:21 +0100

    Facebook Login

--- app/models/User.java
+++ app/models/User.java
@@ -2,9 +2,12 @@ package models;
  
 import java.util.*;
 import javax.persistence.*;
+
+import com.google.gson.JsonObject;
  
 import play.db.jpa.*;
 import play.data.validation.*;
+import play.mvc.Scope.Session;
  
 @Entity
 public class User extends Model {
@@ -26,7 +29,28 @@ public class User extends Model {
         this.fullname = fullname;
     }
     
-    public static User connect(String email, String password) {
+    public User(String email, String fullname) {
+        this.email = email;
+        this.fullname = fullname;
+    }
+    
+    public static void facebookOAuthCallback(JsonObject data){
+    	String email = data.get("email").getAsString();
+    	String fullname = data.get("name").getAsString();
+    	User user = findByEmail(email);
+    	if(user == null){
+    		user = new User(email, fullname);
+    		user.save();
+    	}
+    	Session.current().put("user", user.email);
+    }
+    
+    private static User findByEmail(String email) {
+    	System.out.println(email);
+    	return find("byEmail", email).first();
+	}
+
+	public static User connect(String email, String password) {
         return find("byEmailAndPassword", email, password).first();
     }
     
