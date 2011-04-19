678009dbe44fc4fe3041a4516800d633f06a468f
Sascha Hanke <sascha.hanke@opitz-consulting.com>
2011-04-19 16:07:43 +0200

    Search

--- app/controllers/Search.java
+++ app/controllers/Search.java
@@ -6,16 +6,16 @@ package controllers;
 import java.lang.reflect.Type;
 import java.util.List;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
-
 import models.Info;
 import models.Post;
 import play.modules.search.Query;
 import play.mvc.Before;
 import play.mvc.Controller;
 import utils.StringUtils;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 /**
  * @author azrail
@@ -30,46 +30,39 @@ public class Search extends Controller {
 
 	public static void searchPostsJson(String body) {
 		String searchTerm = StringUtils.chopFirstLast(body);
+		String searchQuery = "title:" + searchTerm + " OR content:" + searchTerm;
 		
-		Query q = play.modules.search.Search.search("title:" + searchTerm + " OR content:" + searchTerm, Post.class);
+		Query q = play.modules.search.Search.search(searchQuery , Post.class);
+
 		List<Post> posts = q.fetch();
 
-		Gson gson =  new GsonBuilder().generateNonExecutableJson().create();
-		Type listType = new TypeToken<List<String>>() {}.getType();
+		Gson gson = new GsonBuilder().generateNonExecutableJson().create();
+		Type listType = new TypeToken<List<Post>>() {
+		}.getType();
 		gson.toJson(posts, listType);
-		
+
+		System.out.println(searchQuery);
+		System.out.println("Hits:" + posts.size());
 		renderJSON(posts);
 	}
-	
+
 	public static void search(String suchString) {
-		
+
 		if (StringUtils.nullCheck(suchString)) {
 			suchString = request.params.allSimple().get("body");
 		}
-		
-		System.out.println(suchString);
-		
-//		if (!StringUtils.nullCheck(autoCompleteModel) && !LongUtils.nullCheck(autoCompleteId)) {
-//			if (autoCompleteModel.equals("models_post")) {
-//				Post post = Post.findById(autoCompleteId);
-//				Application.show(post.id);
-//			}
-//		} else {
-//			Client client = ElasticSearch.client();
-//			SearchResponse response = client.prepareSearch("models_post").setSearchType(SearchType.DFS_QUERY_THEN_FETCH).setQuery(boolQuery().should(termQuery("content", suchString)).should(termQuery("title", suchString))).setFrom(0).setSize(60).setExplain(true).execute().actionGet();
-//			SearchHit[] sh = response.getHits().hits();
-//			List<Post> posts = new ArrayList<Post>();
-//			for (SearchHit searchHit : sh) {
-//				Post post = Post.findById(new Long(searchHit.id()));
-//				posts.add(post);
-//			}
-//
-//			if (posts.size() > 1 || posts.size() == 0) {
-//				Info info = new Info();
-//				render(suchString, posts, info);
-//			} else {
-//				Application.show(posts.get(0).id);
-//			}
-//		}
+
+		String searchTerm = suchString;
+
+		Query q = play.modules.search.Search.search("title:\"*" + searchTerm + "*\" OR content:\"*" + searchTerm + "*\"", Post.class);
+
+		List<Post> posts = q.fetch();
+
+		if (posts.size() > 1 || posts.size() == 0) {
+			Info info = new Info();
+			render(suchString, posts, info);
+		} else {
+			Application.show(posts.get(0).id);
+		}
 	}
 }
